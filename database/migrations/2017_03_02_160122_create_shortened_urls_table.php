a238bc411fe2baf1425f3586628be311bd9797da
Haries Nur Ikhwan <hariesnurikhwan@gmail.com>
2017-03-03 00:13:59 +0800

    Add ShortenedUrl model.

---
+++ database/migrations/2017_03_02_160122_create_shortened_urls_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShortenedUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shortened_urls', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsinged()->nullable();
+            $table->string('alias', 50)->unique();
+            $table->string('mobile_number', 11);
+            $table->text('text');
+            $table->boolean('enabled')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shortened_urls');
+    }
+}
