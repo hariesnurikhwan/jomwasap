f32dd2cafff5fc0f20239870cc5b70467d833db9
Haries Nur Ikhwan <hariesnurikhwan@gmail.com>
2017-03-03 15:43:35 +0800

    Add show page.

--- resources/views/generate/show.blade.php
+++ resources/views/generate/show.blade.php
@@ -5,9 +5,19 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">
+					View "{{ $url->alias }}" Link Content.
+					<a href="{{ route('generate.edit', $url->id) }}" class="btn btn-success btn-xs pull-right">Edit Link</a>
+                </div>
                 <div class="panel-body">
-
+                    <dl class="dl-horizontal">
+                        <dt>URL</dt>
+                            <dd>https://hi.jomwasap.my/{{ $url->alias }}</dd>
+                        <dt>Mobile Number</dt>
+                            <dd>{{ phone($url->mobile_number, 'MY') }}</dd>
+                        <dt>Pretext Chat</dt>
+                            <dd><pre>{{ $url->text }}</pre></dd>
+                    </dl>
                 </div>
             </div>
         </div>
