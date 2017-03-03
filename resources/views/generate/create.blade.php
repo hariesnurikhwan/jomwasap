59cef234f878f92ebc9078b43179f17940d86397
Haries Nur Ikhwan <hariesnurikhwan@gmail.com>
2017-03-03 16:38:18 +0800

    Update panel heading in generate url page.

--- resources/views/generate/create.blade.php
+++ resources/views/generate/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+                <div class="panel-heading">Generate Clickable WhatsApp Link.</div>
                 <div class="panel-body">
 					{!! Form::open(['method' => 'POST', 'route' => 'generate.store']) !!}
 
