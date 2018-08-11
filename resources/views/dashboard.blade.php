189a330c51ddea7bb7d47c2bb6b41fee474205f9
iserifith <seriputera2@gmail.com>
2018-08-11 19:37:55 +0800

    resolve conflict and remove placeholder stuff

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,43 +19,7 @@
 
         },
         mounted: function() {
-            var ctx = document.getElementById('state').getContext('2d');
-            new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                    datasets: [{
-                        label: ' of Votes',
-                        data: [12, 19, 3, 5, 2, 3],
-                        backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero:true
-                            }
-                        }]
-                    }
-                }
-            });
+
         }
     })
 </script>
@@ -80,11 +44,6 @@
                 <div class="url_count p-2">
                     <p id="total_hit_count" class="url_count_number text-monospace text-center"> @{{ total_hits_count }} </p>
                     <p class="url_count_link text-monospace text-center">Total Hits Count</p>
-                </div>
-            </div>
-            <div class="d-flex flex-row justify-content-start">
-                <div class="p-2">
-                    <canvas id="state"></canvas>
                 </div>
             </div>
         </div>
